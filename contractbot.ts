744a1aa4cb5b3bfd1147120c9ddc4b12223653f2
AslamSDM <aslamsdm@cet.ac.in>
2023-09-27 17:22:53 +0530

    fixed

--- contractbot.ts
+++ contractbot.ts
@@ -1,7 +1,7 @@
 // import {TelegramBot} from "node-telegram-bot-api";
 import TelegramBot from "node-telegram-bot-api";
-import { ethers, Wallet, HDNodeWallet, Contract, parseEther ,formatEther} from "ethers";
-// import axios from "axios";
+import { ethers, Wallet, HDNodeWallet, Contract, parseEther ,formatEther, isAddress} from "ethers";
+import axios from "axios";
 import "dotenv/config";
 import db from "./dbconnect";
 import Users from "./userschema";
@@ -21,33 +21,35 @@ let contraddr:any
 let source:any
 
 bot.onText(/\/start/,async(msg:any)=>{
-    await db()
     chatId = msg.chat.id;
-    const userexist: any = await (Users.find({ userid: msg.from?.username }));
-    message += "Welcome to Contract bot \n"
-    message += "From Abstra \n"
-    message += "\n"
-
-    if (userexist.length > 0) {
-        wallet = new ethers.Wallet((userexist[0].PrivateKey), provider)
-    } else {
+    // await db()
+    // const userexist: any = await (Users.find({ userid: msg.from?.username }));
+    // message += "Welcome to Contract bot \n"
+    // message += "From Abstra \n"
+    // message += "\n"
+    
+    // if (userexist.length > 0) {
+    //     wallet = new ethers.Wallet((userexist[0].PrivateKey), provider)
+    // } else {
+    //     wallet = ethers.Wallet.createRandom()
+    //     const user = new Users({
+        //         userid: msg.from?.username,
+        //         chatID: msg.chat.id,
+        //         address: (wallet.address),
+        //         PrivateKey: (wallet.privateKey)
+        //     })
+        //     await user.save()
+        // }
         wallet = ethers.Wallet.createRandom()
-        const user = new Users({
-            userid: msg.from?.username,
-            chatID: msg.chat.id,
-            address: (wallet.address),
-            PrivateKey: (wallet.privateKey)
-        })
-        await user.save()
-    }
-    const waladr = await wallet.address
-    ethbalance = formatEther(String(await provider.getBalance(waladr)))
-    message += `Your wallet: ${waladr}\n`
-    message += `Your ETH Balance: ${ethbalance}\n`
-    bot.sendMessage(chatId,message,{reply_markup:{
-        inline_keyboard:[
-            [{text:"Deploy Contract" , callback_data:"deploy"}],
-            [{text:"Change Liquidity" , callback_data:"liquidity"}],
+        const waladr = await wallet.address
+        ethbalance = formatEther(String(await provider.getBalance(waladr)))
+        message += `Your wallet: ${waladr}\n`
+        message += `Your ETH Balance: ${ethbalance}\n`
+        bot.sendMessage(chatId,message,{reply_markup:{
+            inline_keyboard:[
+                [{text:`Contract : ${contraddr? contraddr : "Enter Address"}` , callback_data:"contract"}],
+                [{text:"Deploy Contract" , callback_data:"deploy"}],
+                [{text:"Change Liquidity" , callback_data:"liquidity"}],
             [{text:"Verify Contract" , callback_data:"verify"}],
             [{text:"Download Contract Code" , callback_data:"download"}],
             [{text:"Get ABI" , callback_data:"get_abi"}],
@@ -56,53 +58,115 @@ bot.onText(/\/start/,async(msg:any)=>{
         ]
     }})
 })
-function getInput(chatID,text){
-    let mid = 0
-    let input:any
-    bot.sendMessage(chatID,text,{reply_markup:{
-        force_reply:true
-    }}).then((m:any)=>mid=m.message_id)
-    bot.onReplyToMessage(chatID,mid,(m:any)=>{
-        input=m.text
-    })
-    return input
-}
+bot.onText(/\/info/,async(msg:any)=>{
+    bot.sendMessage(chatId,message,{reply_markup:{
+        inline_keyboard:[
+            [{text:`Contract : ${contraddr? contraddr : "Enter Address"}` , callback_data:"contract"}],
+            [{text:"Deploy Contract" , callback_data:"deploy"}],
+            [{text:"Change Liquidity" , callback_data:"liquidity"}],
+        [{text:"Verify Contract" , callback_data:"verify"}],
+        [{text:"Download Contract Code" , callback_data:"download"}],
+        [{text:"Get ABI" , callback_data:"get_abi"}],
+        [{text:"Decompile" , callback_data:"decompile"}],
+        [{text:"Explain Contract (AI🔮)" , callback_data:"explain"}],
+    ]
+    }})
+
+})
+
+
 bot.on("callback_query",async(msg:any)=>{
     const data = msg.data;
-    switch(data){
-        case "deploy":
-            // bot.sendMessage(chatId,"")
-             source = getInput(chatId,"Send the code of the smart contract")
-
-            //Compilation of smartcontract 
-            const {bytecode,abi} = compileContract(source)
-
-            // Create a factory for your contract
-            let factory = new ethers.ContractFactory(abi, bytecode, wallet);
-
-            // Deploy the contract
-            let contract = await factory.deploy();
-
-            break;
-        case "verify":
-             contraddr = getInput(chatId,"Send contract Address")
-             source = getInput(chatId,"Send source code of the contract ")
-            //call the verify endpoint on the etherscan api 
-
-            break;
-        case "decompile":
-             contraddr = getInput(chatId,"Send contract Address")
-             //get the contract byte code and check for similar contracts in database 
-
-
-            break;
-        case "download":
-
-            break;
-        case "download":
-            break;
-        case "download":
-            break;
-
+    if (data=="contract"){
+        let mid = 0;
+        bot.sendMessage(chatId, "Enter Contract Address\n", {
+            reply_markup: {
+                force_reply: true
+            }
+        }).then((msg:any) => {
+            mid = msg.message_id
+            bot.onReplyToMessage(chatId, mid, (msg) => {
+                contraddr = msg.text
+                console.log(contraddr)
+            })
+        });
+    }
+    else if(isAddress(contraddr)){
+        switch(data){
+            case "deploy":
+                // bot.sendMessage(chatId,"")
+                //  source = getInput(chatId,"Send the code of the smart contract")
+    
+                // //Compilation of smartcontract 
+                // const {bytecode,abi} = compileContract(source)
+    
+                // // Create a factory for your contract
+                // let factory = new ethers.ContractFactory(abi, bytecode, wallet);
+    
+                // // Deploy the contract
+                // let contract = await factory.deploy();
+    
+                break;
+            case "verify":
+                //  contraddr = getInput(chatId,"Send contract Address")
+                //  source = getInput(chatId,"Send source code of the contract ")
+                //call the verify endpoint on the etherscan api 
+    
+                break;
+            case "decompile":
+                //  contraddr = getInput(chatId,"Send contract Address")
+                 //get the contract byte code and check for similar contracts in database 
+    
+                break;
+            case "download":
+                // contraddr = await getInput(chatId,"Send contract Address",getCode)
+                const code = await getCode()
+                break;
+            case "get_abi":
+                // contraddr = await getInput(chatId,"Send contract Address",getABI)
+                
+                const abi =await getABI()
+                break;
+    
+            case "explain":
+    
+                break;
+        }
+    }else if(!contraddr){
+        bot.sendMessage(chatId,"Please add the contract address")
     }
 })
+async function getABI(){
+    const abiurl =`https://api.etherscan.io/api?module=contract&action=getabi&address=${contraddr}&apikey=${process.env.ETHERSCAN_KEY}`
+    const getabi = await axios.get(abiurl).then((res:any)=>{
+
+        sendMessage(chatId,`ABI:\n${res.data.result}`)
+    })
+}
+async function getCode() {
+    const url =`https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${contraddr}&apikey=${process.env.ETHERSCAN_KEY}`
+    const getcode = await axios.get(url).then((res:any)=>{
+        sendMessage(chatId,`title: ${res.data.result[0].ContractName},\n Code: \n ${res.data.result[0].SourceCode}`)
+    })
+    
+}
+async function sendMessage(chatId: number | string, message: string, options?: TelegramBot.SendMessageOptions): Promise<void> {
+    const MAX_MESSAGE_LENGTH = 4096;
+    let messageToSend = message;
+
+    while (messageToSend.length > 0) {
+        let messageChunk;
+
+        if (messageToSend.length > MAX_MESSAGE_LENGTH) {
+            let lastSpaceIndex = messageToSend.slice(0, MAX_MESSAGE_LENGTH).lastIndexOf(' ');
+            lastSpaceIndex = lastSpaceIndex === -1 ? MAX_MESSAGE_LENGTH : lastSpaceIndex;
+            messageChunk = messageToSend.slice(0, lastSpaceIndex);
+            messageToSend = messageToSend.slice(lastSpaceIndex);
+        } else {
+            messageChunk = messageToSend;
+            messageToSend = '';
+        }
+
+        await bot.sendMessage(chatId, messageChunk, options);
+    }
+}
